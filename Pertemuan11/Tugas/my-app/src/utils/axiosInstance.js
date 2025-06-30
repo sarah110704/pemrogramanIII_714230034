4119e41727731a4fd047ead39b00445b3e19599a
sarah110704 <sarahyohana439@gmail.com>
2025-06-30 15:23:47 +0700

    praktikum

--- Pertemuan11/Tugas/my-app/src/utils/axiosInstance.js
+++ Pertemuan11/Tugas/my-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Buat instance axios
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8088/api",
+  baseURL: "https://backendpraktikum-production.up.railway.app/api",
 });
 
 // Interceptor REQUEST – tambah token secara otomatis
